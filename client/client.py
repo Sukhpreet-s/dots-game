810226b69ced73abebd49f627e76cb95c582b866
Sukhpreet-s <41968447+Sukhpreet-s@users.noreply.github.com>
2023-11-28 02:52:20 +0000

    load server endpoint from env var

--- client/client.py
+++ client/client.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env if present
+load_dotenv()
 
 app = Flask(__name__)
 
+SERVER_URL = os.environ.get("SERVER_URL", "localhost:5000")
+print("SERVER_URL: ", SERVER_URL)
+
 @app.route('/')
 def hello_world():
-    return render_template("test-client.html")
+    return render_template("test-client.html", server_url=SERVER_URL)
 
 
 if __name__ == '__main__':
